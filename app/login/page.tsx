192d6c21e60ba0a6a3bf2b42635b61174cd482e8
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2026-06-24 12:38:49 +0200

    feat: implement login page with profile view component

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { ProfileView } from '@/components/netscore/profile-view'
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-dvh items-center justify-center bg-background">
+      <ProfileView />
+    </div>
+  )
+}
